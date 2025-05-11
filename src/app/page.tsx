8cea03b69f5ee4d7fa8528bbf2537b82dbdfd08a
Jack <jackkeirnan@gmail.com>
2025-05-11 21:05:31 +0100

    Initial Creation of Project

---
+++ src/app/page.tsx
@@ -0,0 +1,32 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <div> Fattorio Cercatoia</div>
+      </Head>
+
+      <section className='min-h-screen bg-neutral-600'>
+
+      </section>
+
+      <section className='min-h-screen bg-neutral-500'>
+
+      </section>
+
+      <section className='min-h-screen bg-neutral-400'>
+
+      </section>
+
+      <section className='min-h-screen bg-neutral-300'>
+
+      </section>
+
+      <section className='min-h-screen bg-neutral-200'>
+
+      </section>
+
+    </div>
+  );
+}
